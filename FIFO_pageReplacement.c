9c31037cbf47fabd3720526ce0169c91a00f478f
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-05-03 12:22:42 +0530

    Update FIFO_pageReplacement.c

--- FIFO_pageReplacement.c
+++ FIFO_pageReplacement.c
@@ -1,3 +1,75 @@
+#include <stdio.h>
+
+int main() {
+    int no_frame, no_request, i, pgf = 0, j = 0;
+
+    printf("Enter the number of requests: ");
+    scanf("%d", &no_request);
+
+    printf("Enter the number of frames: ");
+    scanf("%d", &no_frame);
+
+    int frame[no_frame], req[no_request];
+
+    for (i = 0; i < no_frame; i++)
+        frame[i] = -1;
+
+    printf("Enter the requests:\n");
+    for (i = 0; i < no_request; i++) {
+        scanf("%d", &req[i]);
+    }
+
+    printf("Page replacement:\n");
+
+    for (i = 0; i < no_request; i++) {
+        int avail = 0;
+        printf("%d: ", req[i]);
+
+        for (int a = 0; a < no_frame; a++) {
+            if (frame[a] == req[i]) {
+                avail = 1;
+                break;
+            }
+        }
+
+        if (avail == 0) {
+           
+            frame[j] = req[i];
+            j = (j + 1) % no_frame;
+            pgf++;
+        }
+
+        
+        for (int a = 0; a < no_frame; a++) {
+            if (frame[a] != -1)
+                printf("%d ", frame[a]);
+            else
+                printf("x ");
+        }
+        printf("\n");
+    }
+    printf("Total page faults = %d\n", pgf);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 #include<stdio.h>
 void main(){
   int no_frame,no_request,i,pgf=0,j=0;
@@ -55,7 +127,7 @@ void main(){
 
 
 
-/*
+
 
 #include<stdio.h>
 void main(){
