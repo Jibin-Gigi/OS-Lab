d5118d9aa77553a7469c031cfe49cd68ecb7c92e
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-22 01:02:46 +0530

    Update FIFO_pageReplacement.c

--- FIFO_pageReplacement.c
+++ FIFO_pageReplacement.c
@@ -1,153 +1,60 @@
 #include <stdio.h>
 
 int main() {
-    int no_frame, no_request, i, pgf = 0, j = 0;
+    int numFrames, numRequests, i, pageFaults = 0, currentIndex = 0;
 
     printf("Enter the number of requests: ");
-    scanf("%d", &no_request);
+    scanf("%d", &numRequests);
 
     printf("Enter the number of frames: ");
-    scanf("%d", &no_frame);
+    scanf("%d", &numFrames);
 
-    int frame[no_frame], req[no_request];
+    int frames[numFrames], requests[numRequests];
 
-    for (i = 0; i < no_frame; i++)
-        frame[i] = -1;
+    for (i = 0; i < numFrames; i++)
+        frames[i] = -1;
 
     printf("Enter the requests:\n");
-    for (i = 0; i < no_request; i++) {
-        scanf("%d", &req[i]);
+    for (i = 0; i < numRequests; i++) {
+        scanf("%d", &requests[i]);
     }
 
     printf("Page replacement:\n");
 
-    for (i = 0; i < no_request; i++) {
-        int avail = 0;
-        printf("%d: ", req[i]);
+    for (i = 0; i < numRequests; i++) {
+        int available = 0;
+        printf("%d : ", requests[i]);
 
-        for (int a = 0; a < no_frame; a++) {
-            if (frame[a] == req[i]) {
-                avail = 1;
+        for (int j = 0; j < numFrames; j++) {
+            if (frames[j] == requests[i]) {
+                available = 1;
                 break;
             }
         }
 
-        if (avail == 0) {
-           
-            frame[j] = req[i];
-            j = (j + 1) % no_frame;
-            pgf++;
+        if (available == 0) {
+            frames[currentIndex] = requests[i];
+            currentIndex = (currentIndex + 1) % numFrames;
+            pageFaults++;
+
+            for (int j = 0; j < numFrames; j++) {
+                if (frames[j] != -1)
+                    printf("%d ", frames[j]);
+                else
+                    printf("x ");
+            }
+        } else {
+            for (int j = 0; j < numFrames; j++) {
+                if (frames[j] != -1)
+                    printf("%d ", frames[j]);
+                else
+                    printf("x ");
+            }
         }
 
-        
-        for (int a = 0; a < no_frame; a++) {
-            if (frame[a] != -1)
-                printf("%d ", frame[a]);
-            else
-                printf("x ");
-        }
         printf("\n");
     }
-    printf("Total page faults = %d\n", pgf);
+    printf("Total page faults = %d\n", pageFaults);
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include<stdio.h>
-void main(){
-  int no_frame,no_request,i,pgf=0,j=0;
-  printf("Enter the number of request\n");
-  scanf("%d",&no_request);
-  printf("Enter the number of frames\n");
-  scanf("%d",&no_frame);
-   int frame[no_frame],req[no_request];
-  for(i=0;i<no_frame;i++)
-      frame[i]=-1;
-  printf("Enter the requests:\n");
-  for(i=0;i<no_request;i++){
-    scanf("%d",&req[i]);
-  }
-  printf("page replacement :\n");
-  for(i=0;i<no_request;i++){
-    int avail=0;
-    printf("%d :",req[i]);
-    for(int a=0;a<no_frame;a++){
-     
-       if(frame[a]==req[i]){
-         avail=1;
-         break;
-       }
-    }
-    if(avail==0){
-       frame[j]=req[i];
-       j=(j+1)%no_frame;
-       pgf++;
-    }
-    for(int a=0;a<no_frame;a++){
-      printf("%d\t",frame[a]);
-    }
-    printf("\n");
-  }
-  printf("No of page fault=%d\n",pgf);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include<stdio.h>
-void main(){
-	int pageFault=0,j=0;
-	int req;
-	printf("Enter the no.of requests: ");
-	scanf("%d",&req);
-	int frames;
-	printf("Enter the no.of frames: ");
-	scanf("%d",&frames);
-	int requests[req];
-	printf("Enter the requets: ");
-	for(int i=0;i<req;i++){
-		scanf("%d",&requests[i]);
-		printf("  ");
-	}
-	for(int i=0;i<req;i++){
-		printf
-	}
-}
-
-
-*/
