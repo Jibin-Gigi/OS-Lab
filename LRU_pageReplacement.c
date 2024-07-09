f7d36f0b1953a8c0df2ab6a979f5458064ae2338
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-07-09 18:28:56 +0530

    Update LRU_pageReplacement.c

--- LRU_pageReplacement.c
+++ LRU_pageReplacement.c
@@ -1,55 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int numFrames, numRequests, i, pageFaults = 0, currentIndex = 0;
+void main()
+{
+    int no_frames, no_requests, page_faults=0;
+    printf("Enter the no. of frames :");
+    scanf("%d", &no_frames);
 
-    printf("Enter the number of requests: ");
-    scanf("%d", &numRequests);
+    printf("Enter no.of requests :");
+    scanf("%d", &no_requests);
 
-    printf("Enter the number of frames: ");
-    scanf("%d", &numFrames);
+    int frames[no_frames],requests[no_requests],recently_used_index[no_frames];
 
-    int frames[numFrames], requests[numRequests], used[numFrames];
-
-    for (i = 0; i < numFrames; i++) {
-        frames[i] = -1;
-        used[i] = 0;
+    for (int i =0;i<no_frames;i++){
+        frames[i]=-1;
+        recently_used_index[i]= -1;
     }
 
-    printf("Enter the requests:\n");
-    for (i = 0; i < numRequests; i++) {
+    printf("Enter the requests: ");
+    for (int i=0; i<no_requests; i++){
         scanf("%d", &requests[i]);
     }
 
-    printf("Page replacement (LRU):\n");
-
-    for (i = 0; i < numRequests; i++) {
-        int available = 0;
+    for (int i =0;i<no_requests;i++){
         printf("%d: ", requests[i]);
-
-        for (int j = 0; j < numFrames; j++) {
-            if (frames[j] == requests[i]) {
+        int available = 0;
+        for (int j =0;j<no_frames;j++){
+            if (frames[j]==requests[i]){
                 available = 1;
-                used[j] = i + 1;
+                recently_used_index[j] = i;
                 break;
             }
         }
 
-        if (available == 0) {
-            int leastUsedIndex = 0;
-            for (int j = 1; j < numFrames; j++) {
-                if (used[j] < used[leastUsedIndex])
-                    leastUsedIndex = j;
+        if (available == 0){
+            int least_used_index = 0;
+            for (int j = 1;j<no_frames;j++){
+                if (recently_used_index[j] < recently_used_index[least_used_index])
+                    least_used_index = j;
             }
 
-            frames[leastUsedIndex] = requests[i];
-            used[leastUsedIndex] = i + 1;
-            pageFaults++;
+            frames[least_used_index] = requests[i];
+            page_faults++;
+            recently_used_index[least_used_index] = i;
+
         }
 
-
-
-        for (int j = 0; j < numFrames; j++) {
+         for (int j = 0; j < no_frames; j++) {
             if (frames[j] != -1)
                 printf("%d ", frames[j]);
             else
@@ -57,9 +53,6 @@ int main() {
         }
         printf("\n");
     }
-    printf("Total page faults = %d\n", pageFaults);
-    printf("Total page hits = %d\n", numRequests - pageFaults); 
 
-
-    return 0;
-}
+    printf("Total page faults: %d\n", page_faults);
+}    
